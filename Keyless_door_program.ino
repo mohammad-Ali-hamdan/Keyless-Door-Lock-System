408d06b8670154b8de88fec350cecfbc62d5503f
Mohammad Ali Hamdan <mohaliassadhamdan@gmail.com>
2020-11-08 11:08:32 +0000

    Add files via upload

---
+++ Keyless_door_program.ino
@@ -0,0 +1,261 @@
+#include <Keypad.h>
+
+#include<LiquidCrystal.h>
+
+#include<EEPROM.h>
+
+LiquidCrystal liquid_crystal_display(9,8,7,6,5,4);
+
+char password[4];
+
+char initial_password[4],new_password[4];
+
+int i=0;
+
+int relay_pin = 2;
+
+char key_pressed=0;
+
+const byte rows = 4; 
+
+const byte columns = 3; 
+char hexaKeys[rows][columns] = {
+
+{'1','2','3'},
+
+{'4','5','6'},
+
+{'7','8','9'},
+
+{'*','0','#'}
+
+};
+
+byte row_pins[rows] = {A0,A1,A2,A3};
+
+byte column_pins[columns] = {A4,A5,3}; 
+Keypad keypad_key = Keypad( makeKeymap(hexaKeys), row_pins, column_pins, rows, columns);
+
+void setup()
+
+{
+
+  pinMode(relay_pin, OUTPUT);
+
+  liquid_crystal_display.begin(16,2);
+
+  liquid_crystal_display.print("Moh HAMDAN");
+
+  liquid_crystal_display.setCursor(0,1);
+
+  liquid_crystal_display.print("Keyless door ");
+
+  delay(2000);
+
+  liquid_crystal_display.clear();
+
+  liquid_crystal_display.print("Enter Password");
+
+  liquid_crystal_display.setCursor(0,1);
+
+  initialpassword();
+
+}
+
+
+void loop()
+{
+
+  digitalWrite(relay_pin, LOW);
+  key_pressed = keypad_key.getKey();
+  if(key_pressed=='#')
+      change();
+  if(key_pressed)
+  {
+    password[i++]=key_pressed;
+    liquid_crystal_display.print(key_pressed);
+    
+  }
+  if(i==4)
+  {
+    delay(200);
+    for (int j=0;j<4;j++)
+    {
+      initial_password[j]= EEPROM.read(j);
+    }
+    if(!(strncmp(password , initial_password,4)))
+    {
+      liquid_crystal_display.clear();
+
+      liquid_crystal_display.print("Pass Accepted");
+
+      digitalWrite(relay_pin, HIGH);
+
+      delay(2000);
+
+      liquid_crystal_display.setCursor(0,1);
+
+      liquid_crystal_display.print("Pres # to change");
+
+      delay(2000);
+
+      liquid_crystal_display.clear();
+
+      liquid_crystal_display.print("Enter Password:");
+
+      liquid_crystal_display.setCursor(0,1);
+
+      i=0;
+      
+      
+    }
+
+    else
+    {
+      liquid_crystal_display.clear();
+
+      liquid_crystal_display.print("Wrong Password");
+
+      liquid_crystal_display.setCursor(0,1);
+
+      liquid_crystal_display.print("Pres # to Change");
+
+      delay(2000);
+
+      liquid_crystal_display.clear();
+
+      liquid_crystal_display.print("Enter Password");
+
+      liquid_crystal_display.setCursor(0,1);
+
+      i=0;
+
+      
+    }
+  }
+  
+  
+}
+
+
+
+
+
+void change()
+
+{
+
+  int j=0;
+
+  liquid_crystal_display.clear();
+
+  liquid_crystal_display.print("Current Password");
+
+  liquid_crystal_display.setCursor(0,1);
+
+  while(j<4)
+
+  {
+
+    char key=keypad_key.getKey();
+
+    if(key)
+
+    {
+
+      new_password[j++]=key;
+
+      liquid_crystal_display.print(key);
+
+       
+
+    }
+
+    key=0;
+
+  }
+
+  delay(500);
+
+
+
+
+  if((strncmp(new_password, initial_password, 4)))
+
+  {
+
+    liquid_crystal_display.clear();
+
+    liquid_crystal_display.print("Wrong Password");
+
+    liquid_crystal_display.setCursor(0,1);
+
+    liquid_crystal_display.print("Try Again");
+
+    delay(1000);
+
+  }
+
+  else
+
+  {
+
+    j=0;
+
+    liquid_crystal_display.clear();
+
+    liquid_crystal_display.print("New Password:");
+
+    liquid_crystal_display.setCursor(0,1);
+
+    while(j<4)
+
+    {
+
+      char key=keypad_key.getKey();
+
+      if(key)
+
+      {
+
+        initial_password[j]=key;
+
+        liquid_crystal_display.print(key);
+
+        EEPROM.write(j,key);
+
+        j++;
+
+     
+
+      }
+
+    }
+
+    liquid_crystal_display.print("Pass Changed");
+
+    delay(1000);
+
+  }
+
+  liquid_crystal_display.clear();
+
+  liquid_crystal_display.print("Enter Password");
+
+  liquid_crystal_display.setCursor(0,1);
+
+  key_pressed=0;
+
+}
+
+void initialpassword(){  // initial password 0000
+
+  for(int j=0;j<4;j++)
+
+    EEPROM.write(j, 0+49);
+
+  for(int j=0;j<4;j++)
+
+    initial_password[j]=EEPROM.read(j);
+
+} 
